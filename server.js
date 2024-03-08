2b2b11a09c36168e6dbdf94bf4d65a92ca1fbced
Tom Fishwick <fishwickt@gmail.com>
2024-03-08 20:49:45 +0000

    Convert to module and install prettier

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+import express from 'express';
+import pkg from 'body-parser';
+import cors from 'cors';
+import { config } from 'dotenv';
+import { connect, Schema as _Schema, model } from 'mongoose';
 
-const cors = require('cors')
+const { urlencoded } = pkg;
 
-//Need dotenv for testing in windows
-require('dotenv').config();
+// Need dotenv for testing in windows
+config();
 
-const mongoose = require('mongoose')
-//mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
-// mongoose.connect(process.env.MLAB_URI, {useNewUrlParser: true}, function (err) {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("Connected");
-//   }
-// });
-mongoose.connect(process.env.MLAB_URI).then(console.log('Connected')).catch(ex => console.error(ex));
-app.use(cors())
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
-//app.use(bodyParser.json())
+connect(process.env.MLAB_URI)
+    .then(console.log('Connected'))
+    .catch((ex) => console.error(ex));
 
+app.use(cors());
 
-app.use(express.static('public'))
+app.use(urlencoded({ extended: false }));
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html');
 });
-
 
 // Not found middleware
 /*app.use((req, res, next) => {
@@ -35,137 +29,161 @@ app.get('/', (req, res) => {
 })*/
 
 // Error Handling middleware
-app.use((err, req, res) => {
-  let errCode, errMessage
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+    let errCode, errMessage;
 
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
+    if (err.errors) {
+        // mongoose validation error
+        errCode = 400; // bad request
+        const keys = Object.keys(err.errors);
+        // report the first validation error
+        errMessage = err.errors[keys[0]].message;
+    } else {
+        // generic or custom error
+        errCode = err.status || 500;
+        errMessage = err.message || 'Internal Server Error';
+    }
+    res.status(errCode).type('txt').send(errMessage);
+});
 
-//Schemas
-const Schema = mongoose.Schema;
-//exUser
+// Schemas
+const Schema = _Schema;
+// exUser
 const exUserSchema = Schema({
-  usrName: { type: String, required: true }
+    usrName: { type: String, required: true },
 });
 
-const exUser = mongoose.model('exUser', exUserSchema);
+const exUser = model('exUser', exUserSchema);
 
-//Exercises
+// Exercises
 const exerciseSchema = Schema({
-  exUser: {type: mongoose.Schema.Types.ObjectId, ref: 'exUser', required: true},
-  Description: {type:String, required: true},
-  Duration: {type:Number, required: true},
-  ExDate: Date
+    exUser: {
+        type: _Schema.Types.ObjectId,
+        ref: 'exUser',
+        required: true,
+    },
+    Description: { type: String, required: true },
+    Duration: { type: Number, required: true },
+    ExDate: Date,
 });
 
-const exercise = mongoose.model('exercise', exerciseSchema);
+const exercise = model('exercise', exerciseSchema);
 
-//Create a New User
-app.post('/api/exercise/new-user', function (req, res){
-  console.log(req.body.username);
-  //res.json({"username": req.body.username});
-  exUser.create({usrName: req.body.username}, function (err, data){
-    if (err) {
-      console.log(err);
-    } else {
-      res.json({"username": req.body.username, "_id": data._id});
-    }
-  });
+// Create a New User
+app.post('/api/exercise/new-user', function (req, res) {
+    console.log(req.body.username);
+    exUser
+        .create({ usrName: req.body.username })
+        .then((data) =>
+            res.json({ username: req.body.username, _id: data._id })
+        )
+        .catch((ex) => console.error(ex));
 });
 
-//Get array of all users
-app.get("/api/exercise/users", function (req, res){
-  exUser.find({}).exec(function (err, docs) {
-    if (err) {
-      console.log(err);
-      res.send(err);
-    } else {
-      res.send(docs);
-    }
-  });
+// Get array of all users
+app.get('/api/exercise/users', function (req, res) {
+    exUser
+        .find({})
+        .then((docs) => res.send(docs))
+        .catch((err) => {
+            console.error(err);
+            res.send(err);
+        });
 });
 
-//Add exercises
-app.post("/api/exercise/add", function (req, res) {
-  //console.log(req.body);
-  //check the user id is real first
-  exUser.findById(req.body.userId).exec(function (err, usr) {
-    if (err) {
-      console.log(err);
-      res.send("unknown _id");
-    } else {
-      var d = new Date();
-      console.log(usr);
-      if (req.body.Date !== undefined) {
-        d = req.body.Date;
-      }
-      exercise.create({exUser: req.body.userId, Description: req.body.description, Duration: req.body.duration, ExDate: d}, function (err){
-        if (err) {
-          console.log(err);
-          res.send(err);
-        } else {
-          res.json({"username": usr.usrName, "description": req.body.description, "duration": req.body.duration, "_id": req.body.userId, "date": d.toDateString()});
-        }
-      });
-    }
-  });
+// Add exercises
+app.post('/api/exercise/add', function (req, res) {
+    // console.log(req.body);
+    // check the user id is real first
+    exUser
+        .findById(req.body.userId)
+        .then((usr) => {
+            let d = new Date();
+            if (req.body.Date) {
+                d = req.body.Date;
+            }
+
+            exercise
+                .create({
+                    exUser: req.body.userId,
+                    Description: req.body.description,
+                    Duration: req.body.duration,
+                    ExDate: d,
+                })
+                .then(() => {
+                    res.json({
+                        username: usr.usrName,
+                        description: req.body.description,
+                        duration: req.body.duration,
+                        _id: req.body.userId,
+                        date: d.toDateString(),
+                    });
+                })
+                .catch((ex) => {
+                    console.error(ex);
+                    res.send(ex);
+                });
+        })
+        .catch((err) => {
+            console.error(err);
+            res.send('unknown _id');
+        });
 });
 
-//exercise log
-app.get("/api/exercise/log", function (req, res) {
-  exUser.findById(req.query.userId).exec(function (err, usr) {
-    if (err) {
-      console.log(err);
-      res.send("unknown _id");
-    } else {
-      var query ={};
-      //if (req.query.userId !== "") {
-        query["exUser"] = req.query.userId;
-      //}
-      if (req.query.from !== undefined && req.query.to == undefined) {
-        query["ExDate"] = {"$gte" : req.query.from};
-      }
-      if (req.query.to !== undefined && req.query.from == undefined) {
-        query["ExDate"] = {"$lte" : req.query.to};
-      }
-      if (req.query.from !== undefined && req.query.to !== undefined) {
-        query["ExDate"] = {"$gte": req.query.from, "$lte": req.query.to};
-      }
-      console.log(query);
-      console.log(req.query);
+/**
+ * Check if this is a valid date
+ * @param {string} date
+ * @returns {boolean}
+ */
+function IsDate(date) {
+    return new Date(date) !== 'Invalid Date' && !isNaN(new Date(date));
+}
 
-      var exQuery = exercise.find(query);
-      if (req.query.limit !== undefined) {
-        var l = parseInt(req.query.limit);
-        exQuery.limit(l);
-      }
-      exQuery.exec(function (err, data) {
-        if (err) {
-          res.send(err);
-        } else {
-          res.json({"_id": req.query.userId, "username": usr.usrName, "count": data.length, "log": data});
-        }
-      });
+// exercise log
+app.get('/api/exercise/log', function (req, res) {
+    exUser
+        .findById(req.query.userId)
+        .then((usr) => {
+            const query = {
+                exUser: req.query.userId,
+            };
 
-    }
-  });
+            if (
+                req.query.from &&
+                req.query.to &&
+                IsDate(req.query.from) &&
+                IsDate(req.query.to)
+            ) {
+                query['ExDate'] = { $gte: req.query.from, $lte: req.query.to };
+            } else if (req.query.from && IsDate(req.query.from)) {
+                query['ExDate'] = { $gte: req.query.from };
+            } else if (req.query.to && IsDate(req.query.to)) {
+                query['ExDate'] = { $lte: req.query.to };
+            }
 
+            console.log(query);
 
-  
+            const exQuery = exercise.find(query);
+            if (req.query.limit) {
+                const limit = parseInt(req.query.limit);
+                exQuery.limit(limit);
+            }
+
+            exQuery
+                .then((data) =>
+                    res.json({
+                        _id: req.query.userId,
+                        username: usr.usrName,
+                        count: data.length,
+                        log: data,
+                    })
+                )
+                .catch((ex) => res.send(ex));
+        })
+        .catch(() => res.send('unknown _id'));
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+    console.log('Your app is listening on port ' + listener.address().port);
+});
